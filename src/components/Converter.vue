3591ed33b86cf0116dcbeb2a1c0220a344186c71
corvus <vip.egor.voronov.2016@mail.ru>
2023-06-18 14:32:35 +0300

    refactor: move all converter logic to storе

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -1,60 +1,44 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios from 'axios'
+import { useCurrencyStore } from '@/stores/currencyStore'
 
-const responseValute = ref(null)
-const loadingError = ref(false)
+const currencyStore = useCurrencyStore()
 
-const selectedCurrency = ref('USD')
-const selectedCurrencyAmount = ref<number | string>('0')
-const baseCurrencyAmount = ref<number | string>('0')
-
-axios
-  .get('https://www.cbr-xml-daily.ru/daily_json.js')
-  .then((response) => {
-    responseValute.value = response.data.Valute
-  })
-  .catch((error) => {
-    console.log(error)
-    loadingError.value = true
-  })
-
-const convertCurrency = (): void => {
-  const selectedCurrencyValue = parseFloat(responseValute.value[selectedCurrency.value].Value)
-  baseCurrencyAmount.value = +(selectedCurrencyAmount.value * selectedCurrencyValue).toFixed(2)
-}
-
-const convertBaseCurrency = (): void => {
-  const selectedCurrencyValue = parseFloat(responseValute.value[selectedCurrency.value].Value)
-  selectedCurrencyAmount.value = +(baseCurrencyAmount.value / selectedCurrencyValue).toFixed(2)
-}
+currencyStore.fetchData()
 </script>
 
 <template>
   <div class="converterContainer">
     <h1 class="converterHeading">Конвертер валют</h1>
-    <div v-if="loadingError">Ошибка при загрузке данных.</div>
+    <div v-if="currencyStore.loadingError">Ошибка при загрузке данных.</div>
     <div v-else>
-      <div v-if="responseValute === null">Loading...</div>
+      <div v-if="currencyStore.responseValute === null">Loading...</div>
       <div v-else>
         <input
           type="number"
           class="converterInput"
-          v-model="selectedCurrencyAmount"
+          v-model="currencyStore.selectedCurrencyAmount"
           placeholder="Введите сумму"
-          @input="convertCurrency"
+          @input="currencyStore.convertCurrency"
           @focus="$event.target.select()"
         />
-        <select class="converterSelect" v-model="selectedCurrency" @change="convertCurrency">
-          <option v-for="(valute, key) in responseValute" :key="key" :value="valute.CharCode">
+        <select
+          class="converterSelect"
+          v-model="currencyStore.selectedCurrency"
+          @change="currencyStore.convertCurrency"
+        >
+          <option
+            v-for="(valute, key) in currencyStore.responseValute"
+            :key="key"
+            :value="valute.CharCode"
+          >
             {{ valute.CharCode }} ({{ valute.Name }})
           </option>
         </select>
         <input
           type="number"
           class="converterInput"
-          v-model="baseCurrencyAmount"
-          @input="convertBaseCurrency"
+          v-model="currencyStore.baseCurrencyAmount"
+          @input="currencyStore.convertBaseCurrency"
           @focus="$event.target.select()"
         />
         ₽
@@ -64,17 +48,14 @@ const convertBaseCurrency = (): void => {
 </template>
 
 <style scoped lang="scss">
-.converter {
-  &Container {
-    background-color: red;
-    width: 16rem;
-    height: auto;
-  }
-  &Input {
-    &[type='number']::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-  }
+.converterContainer {
+  background-color: red;
+  width: 16rem;
+  height: auto;
+}
+
+.converterInput[type='number']::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
 }
 </style>
---
+++ src/stores/currencyStore.ts
@@ -0,0 +1,57 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios'
+
+interface Valute {
+  [key: string]: {
+    Value: string
+  }
+}
+
+export const useCurrencyStore = defineStore('currency', {
+  state: () => ({
+    responseValute: ref<Valute | null>(null),
+    loadingError: ref(false),
+    selectedCurrency: ref(localStorage.getItem('selectedCurrency') || 'USD'),
+    selectedCurrencyAmount: ref<number | string>(
+      localStorage.getItem('selectedCurrencyAmount') || '0'
+    ),
+    baseCurrencyAmount: ref<number | string>(localStorage.getItem('baseCurrencyAmount') || '0')
+  }),
+
+  actions: {
+    async fetchData() {
+      try {
+        const response = await axios.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        this.responseValute = response.data.Valute
+      } catch (error) {
+        console.log(error)
+        this.loadingError = true
+      }
+    },
+
+    convertCurrency() {
+      if (this.responseValute !== null) {
+        const selectedCurrencyValue = parseFloat(this.responseValute[this.selectedCurrency].Value)
+        const selectedCurrencyAmount = Number(this.selectedCurrencyAmount)
+        this.baseCurrencyAmount = +(selectedCurrencyAmount * selectedCurrencyValue).toFixed(2)
+        this.saveDataToStorage()
+      }
+    },
+
+    convertBaseCurrency() {
+      if (this.responseValute !== null) {
+        const selectedCurrencyValue = parseFloat(this.responseValute[this.selectedCurrency].Value)
+        const baseCurrencyAmount = Number(this.baseCurrencyAmount)
+        this.selectedCurrencyAmount = +(baseCurrencyAmount / selectedCurrencyValue).toFixed(2)
+        this.saveDataToStorage()
+      }
+    },
+
+    saveDataToStorage() {
+      localStorage.setItem('selectedCurrency', this.selectedCurrency)
+      localStorage.setItem('selectedCurrencyAmount', this.selectedCurrencyAmount.toString())
+      localStorage.setItem('baseCurrencyAmount', this.baseCurrencyAmount.toString())
+    }
+  }
+})
