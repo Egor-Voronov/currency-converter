db682329557019751b8bc838617299b979ebe369
corvus <vip.egor.voronov.2016@mail.ru>
2023-06-18 12:50:04 +0300

    init: setup application

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Converter from "@/components/Converter.vue";
+import './index.scss'
+</script>
+
+<template>
+  <div class="container">
+      <Converter />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+</style>
